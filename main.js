76cb597b9683d9e0822604bab01b797c5156eeef
Brandon Benefield <bsquared18@gmail.com>
2018-04-05 16:52:23 -0400

    complete fluid layout

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('header nav');
-const navLinks = document.querySelectorAll('nav a');
+// const hamburger = document.querySelector('.hamburger');
+// const nav = document.querySelector('header nav');
+// const navLinks = document.querySelectorAll('nav a');
 
-hamburger.addEventListener('click', () => {
-  if (nav.classList.contains('nav--show')) {
-    nav.classList.remove('nav--show');
-  }
-  else {
-    nav.classList.add('nav--show');
-  }
+// hamburger.addEventListener('click', () => {
+//   if (nav.classList.contains('nav--show')) {
+//     nav.classList.remove('nav--show');
+//   }
+//   else {
+//     nav.classList.add('nav--show');
+//   }
   
-});
+// });
